382e09b5ae387c94c07027f755b1f75dca5c0642
SpartificialUdemy <instructors@spartificial.com>
2024-02-12 01:10:08 +0530

    Modified the setup_simulation function

--- M5 - Earth's Orbit around the Sun/main.py
+++ M5 - Earth's Orbit around the Sun/main.py
@@ -4,14 +4,14 @@ import numpy as np
 
 def setup_simulation(config):
     # Access configuration values
-    planet_name = config['planet_info']['name']
+    planet_name = config['planet_info']['name'] 
     color_at_perihelion = config['planet_info']['perihelion_color']
     color_at_aphelion = config['planet_info']['aphelion_color']
-    initial_position = np.array(config['initial_conditions']['r_0'])
-    initial_velocity = np.array(config['initial_conditions']['v_0'])
-    time_step = config['time_settings']['dt']
-    max_time = config['time_settings']['t_max']
-    method_integration = config['numerical_integration']['method']
+    initial_position = np.array(config['initial_conditions']['position_at_perihelion'])*1e9 # m
+    initial_velocity = np.array(config['initial_conditions']['velocity_at_perihelion'])*1e3 # m/s
+    time_step = config['time_settings']['time_step'] # sec
+    max_time = config['time_settings']['simulation_time']*24*3600 # sec
+    method_integration = config['numerical_integration']['method'] 
 
     # Time array for numerical solution
     t = np.arange(0, max_time, time_step)
@@ -21,7 +21,7 @@ def setup_simulation(config):
     v = np.empty(shape=(len(t), 2))
 
     # Set initial conditions for position and velocity
-    r[0], v[0] = initial_position, initial_velocity
+    r[0], v[0] = np.array([initial_position, 0]), np.array([0, -initial_velocity])
 
     return planet_name, color_at_perihelion, color_at_aphelion, r, v, t, time_step, method_integration
 
