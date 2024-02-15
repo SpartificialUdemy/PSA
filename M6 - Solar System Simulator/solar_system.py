34692c06b8c55cff6524d8c1050b344f57e9f3e2
SpartificialUdemy <instructors@spartificial.com>
2024-02-15 14:12:15 +0530

    Added pygame window for simulation

--- M6 - Solar System Simulator/solar_system.py
+++ M6 - Solar System Simulator/solar_system.py
@@ -2,3 +2,33 @@
 import pygame as pg
 import math
 from random import randint
+
+# Initialize Pygame
+pg.init()
+
+# Create Window
+screen_info = pg.display.Info()
+WIDTH = screen_info.current_w
+HEIGHT = screen_info.current_h
+WINDOW = pg.display.set_mode((WIDTH-150, HEIGHT-150))
+pg.display.set_caption('Solar System Simulator')
+
+# Colors
+BLACK = (0, 0, 0)
+
+# Create Simulation
+run=True
+while run:
+
+    WINDOW.fill(BLACK)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run=False
+    
+    pg.display.update()
+
+
+# Quit the Pygame
+pg.quit()
+
