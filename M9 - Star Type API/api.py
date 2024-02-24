b79b6c859c03d14b19c3314648d20cc8502e0989
SpartificialUdemy <instructors@spartificial.com>
2024-02-25 00:10:32 +0530

    Changed the input for prediction function

--- M9 - Star Type API/api.py
+++ M9 - Star Type API/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from star_data import StarProperties, StarTypePrediction
 from predictor import load_model, make_predictions
 import numpy as np
 
@@ -10,12 +11,19 @@ model = load_model('model.pkl')
 def index_route():
     return {"Health" : "Ok"}
 
-@app.post('/predict')
-def prediction(temperature, luminosity, radius, abs_mag):
-    input_features = [[temperature, luminosity, radius, abs_mag]]
+@app.post('/predict', response_model=StarTypePrediction)
+def prediction(sp: StarProperties):
+    input_features = [[sp.temperature, sp.luminosity, sp.radius, sp.abs_mag]]
     predicted_class, probs, classes = make_predictions(model, input_features)
     return {
         'predicted_probabilities' : dict(zip(classes, probs)),
         'predicted_class' : predicted_class,
         'confidence_score' : str(round(np.max(probs),3)*100) + '%'
     }
+
+'''
+Test on Real Star Data Taken from Wikipedia:- 
+Betelgeuse (Supergiant) ~ https://en.wikipedia.org/wiki/Betelgeuse
+Beta Pictoris (Main Seq) ~ https://en.wikipedia.org/wiki/Beta_Pictoris
+Sirus (White Dwarf) ~ https://en.wikipedia.org/wiki/Sirius
+'''
