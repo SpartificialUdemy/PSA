35c3f95935404d1cf04a41385dccd2314844024d
SpartificialUdemy <instructors@spartificial.com>
2024-02-25 02:26:58 +0530

    Added sorted dictionary for predicted probabilities

--- M9 - Star Type API/api.py
+++ M9 - Star Type API/api.py
@@ -15,8 +15,10 @@ def index_route():
 def prediction(sp: StarProperties):
     input_features = [[sp.temperature, sp.luminosity, sp.radius, sp.abs_mag]]
     predicted_class, probs, classes = make_predictions(model, input_features)
+    pred_probs = dict(zip(classes, probs))
+    sorted_pred_probs = dict(sorted(pred_probs.items(), key = lambda item: item[1], reverse=True))
     return {
-        'predicted_probabilities' : dict(zip(classes, probs)),
+        'predicted_probabilities' : sorted_pred_probs,
         'predicted_class' : predicted_class,
         'confidence_score' : str(round(np.max(probs),3)*100) + '%'
     }
